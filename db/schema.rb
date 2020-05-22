b93c49c507792f1790a5dfd43056b4a2315ceb60
長澤　友也 <tomoaya@nagasawatomoyanoMacBook-Air.local>
2020-05-22 10:55:30 +0900

    Add name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200521092129) do
+ActiveRecord::Schema.define(version: 20200522014859) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
