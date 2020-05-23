d82066e38e17bb865d39d759a91b34dee250db7e
長澤　友也 <tomoaya@nagasawatomoyanoMacBook-Air.local>
2020-05-23 10:35:13 +0900

    Add validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
    validates :title , length: { maximum: 32 } , presence: true
    validates :message , length: { maximum: 256 } , presence: true
-   validates :name , length: { maximum: 32 }
+   validates :name , length: { maximum: 10 }
+   validates :wight ,length: { maximum: 3 }
 end
